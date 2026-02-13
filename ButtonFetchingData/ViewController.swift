6758fefd2fa32ae74dec884cf9344943e3993434
TejasvSingh <singhtejasv98@gmail.com>
2026-02-13 08:53:51 -0600

    Initial Commit

---
+++ ButtonFetchingData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ButtonFetchingData
+//
+//  Created by Tejasv Singh on 2/13/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ButtonFetchingDataTests/ButtonFetchingDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonFetchingDataTests.swift
+//  ButtonFetchingDataTests
+//
+//  Created by Tejasv Singh on 2/13/26.
+//
+
+import Testing
+@testable import ButtonFetchingData
+
+struct ButtonFetchingDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
